5f2901e0331b2a219c8d3880a0c26be5e83464f2
jydoong <88875273+jydoong@users.noreply.github.com>
2023-06-01 11:19:18 +0900

    흉부외과 또는 일반외과 의사 목록 출력하기.sql

--- 프로그래머스/unrated/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/unrated/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -3,3 +3,5 @@ SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD,'%Y-%m-%d') AS DATE
 FROM DOCTOR
 WHERE (MCDP_CD = 'CS' or MCDP_CD = 'GS')
 ORDER BY HIRE_YMD DESC, DR_NAME ASC;
+
+-- 정렬 2개 조건으로 지정하고 싶으면 먼저 지정하고 싶은 조건, 그 다음 지정하고 싶은 조건
