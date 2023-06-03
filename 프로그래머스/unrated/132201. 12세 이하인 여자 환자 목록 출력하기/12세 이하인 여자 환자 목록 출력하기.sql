7645c80fe04f5af183a365a40e114a33a0c2195e
jydoong <88875273+jydoong@users.noreply.github.com>
2023-06-03 12:19:09 +0900

    12세 이하인 여자 환자 목록 출력하기.sql

--- 프로그래머스/unrated/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
+++ 프로그래머스/unrated/132201. 12세 이하인 여자 환자 목록 출력하기/12세 이하인 여자 환자 목록 출력하기.sql
@@ -3,3 +3,6 @@ SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE')
 FROM PATIENT
 WHERE AGE <= 12 AND GEND_CD	= 'W'
 ORDER BY AGE DESC, PT_NAME ASC;
+
+-- 결측값 다른 값으로 표시할 때
+-- IFNULL(결측치 있는 열, 대체할 내용)
