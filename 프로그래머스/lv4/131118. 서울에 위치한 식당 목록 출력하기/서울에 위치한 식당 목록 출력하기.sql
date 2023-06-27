a755fad13239a67da6f65c01e3f836b7c5bbffc6
jydoong <88875273+jydoong@users.noreply.github.com>
2023-06-27 22:20:25 +0900

    서울에 위치한 식당 목록 출력하기.sql

--- 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -6,3 +6,6 @@ on info.REST_ID = re.REST_ID
 where info.ADDRESS like '서울%'
 group by info.REST_ID
 order by SCORE desc, info.FAVORITES desc;
+
+#리뷰들 중 평균 점수 내야하니까 group by 해야함
+#소수점 반올림할 때 round(컬럼명, 출력할 소수점 n번째)
