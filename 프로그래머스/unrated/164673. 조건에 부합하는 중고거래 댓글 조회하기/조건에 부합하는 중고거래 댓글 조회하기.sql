f05a661ca8f387d15dda96e469717135acc287a8
jydoong <88875273+jydoong@users.noreply.github.com>
2023-06-27 22:02:03 +0900

    조건에 부합하는 중고거래 댓글 조회하기.sql

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -5,3 +5,7 @@ inner join USED_GOODS_REPLY as r
 on b.BOARD_ID = r.BOARD_ID
 where DATE_FORMAT(b.CREATED_DATE, '%Y-%m') = '2022-10'
 order by r.CREATED_DATE asc, b.TITLE asc;
+
+# 년, 월, 일만 지정하고 싶을 때 data_format 써주기
+# data_format 지정하고 컬럼명 바꾸려면 as 컬럼명 해주기
+# 2022년 10월에 작성된 게시물이니까 board에서 where 조건 적용해주기
