1dab2f09e5a3a69487f530986ee511ef4c6435a4
jydoong <88875273+jydoong@users.noreply.github.com>
2023-09-19 22:11:26 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,33 +1,14 @@
 import sys
-
-n = int(input())
-
-stack = []
-
-for i in range(n):
-    command = sys.stdin.readline().strip()
-
-    if command.split()[0] == 'push':
-        stack.append(command.split()[1])
-
-    elif command == 'top':
-        if not stack:
-            print('-1')
-        else:
-            print(stack[-1])
-
-    elif command == 'size':
+stack=[]
+for i in range(int(input())):
+    c=sys.stdin.readline( ).rstrip( )
+    if 'push' in  c:
+        stack.append(c.split()[1])
+    elif 'pop' == c:
+        print(stack.pop() if stack else -1)
+    elif 'size' == c:
         print(len(stack))
-
-    elif command == 'pop':
-        if not stack:
-            print('-1')
-        else:
-            print(stack[-1])
-            stack.pop()
-
-    elif command == 'empty':
-        if not stack:
-            print('1')
-        else:
-            print('0')
+    elif 'empty' ==c:
+        print(1 if not stack else 0)
+    elif 'top' == c:
+        print(stack[-1] if stack else -1)
