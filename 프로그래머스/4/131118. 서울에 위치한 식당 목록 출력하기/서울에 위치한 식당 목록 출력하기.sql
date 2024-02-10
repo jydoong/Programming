04a4e5d9a6f684c80a927c3a3e1c93c08f920299
jydoong <88875273+jydoong@users.noreply.github.com>
2024-02-10 16:25:50 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,31 +1,8 @@
-select A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS, 
-        round(avg(B.REVIEW_SCORE),2) as SCORE
-from REST_INFO as A
-inner join REST_REVIEW as B
-on A.REST_ID = B.REST_ID
-where A.ADDRESS like '서울%'
-group by A.REST_ID
-order by SCORE desc, A.FAVORITES desc;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 -- 코드를 입력하세요
-#SELECT info.REST_ID, info.REST_NAME, info.FOOD_TYPE, info.FAVORITES, info.ADDRESS, round(avg(re.REVIEW_SCORE), 2) as SCORE
-#from REST_INFO as info
-#inner join REST_REVIEW as re
-#on info.REST_ID = re.REST_ID
-#where info.ADDRESS like '서울%'
-#group by info.REST_ID
-#order by SCORE desc, info.FAVORITES desc;
+SELECT A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS, round(AVG(B.REVIEW_SCORE), 2) as SCORE
+from REST_INFO A
+inner join REST_REVIEW B
+on A.REST_ID = B.REST_ID
+group by A.REST_ID
+having A.ADDRESS like '서울%'
+order by SCORE desc, A.FAVORITES desc;
